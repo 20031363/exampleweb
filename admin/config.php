c512679d21593c9554c80e5ecce2c2588bf1785c
Nerii-Neiro <javiirving915@gmail.com>
2025-08-27 14:30:24 -0600

    Actualización de la página web3

--- admin/config.php
+++ admin/config.php
@@ -3,8 +3,8 @@
 define('SGBD', 'mysql');  
 define('HOST', 'localhost'); 
 define('DB', 'sakila');
-define('USER', '20031363');
-define('PASSWORD', '20031363');
+define('USER', 'sakila');
+define('PASSWORD', '123');
 
 
 ?>
--- views/footer.php
+++ views/footer.php
@@ -1,6 +1,6 @@
-<div style="background-color: rgb(33, 46, 44)!important; color: white; font-weight:bold; text-align:center;justify-content:center;align-items:center;padding-top:3%;padding-bottom:3%;">
-<div class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top" style="margin-left:5%;margin-right:5%;padding: 0%; background-color: rgb(33, 46, 37)!important;">
-    <p class="col-md-4 mb-0 text-body-secondary" style="color:white !important; font-size:1rem !important; font-weight:bold !important; text-align:center !important; background-color: rgb(33, 46, 37)!important;">ConectaTEC 2025 Company, Inc</p>
+<div style="background-color: rgba(51, 45, 119, 1)!important; color: white; font-weight:bold; text-align:center;justify-content:center;align-items:center;padding-top:3%;padding-bottom:3%;">
+<div class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top" style="margin-left:5%;margin-right:5%;padding: 0%; background-color: rgba(51, 45, 119, 1)!important;">
+    <p class="col-md-4 mb-0 text-body-secondary" style="color:white !important; font-size:1rem !important; font-weight:bold !important; text-align:center !important; background-color: rgba(51, 45, 119, 1)!important;">ConectaTEC 2025 Company, Inc</p>
 
     <a href="/" class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
       <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg>
